3195a8eb8565af8c544d35a2458dcc3bc114fd0f
Adebayo Ayowale Gbenga <haydexwisdom@gmail.com>
2024-03-18 13:49:22 +0000

    FRONTEND: Added theme functionalities via tailwind

--- frontend/src/components/Root.jsx
+++ frontend/src/components/Root.jsx
@@ -1,13 +1,21 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+
 
 export default function Root() {
+  const theme = useSelector((state) => state.theme.mode)
+
+  // useEffect(() => { 
+  //   console.log(theme);
+  // }, [theme]);
   return (
-    <>
+    <div className={ `${theme}` }>
       <Header/>
       <Outlet />
       <Footer/>
-    </>
+    </div>
   );
 }
