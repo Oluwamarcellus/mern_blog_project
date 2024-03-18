157efb04982cbb7b19106dbef7acdec273976f67
Adebayo Ayowale Gbenga <haydexwisdom@gmail.com>
2024-03-18 15:14:28 +0000

    BACKEND: Completed Database setups and DOTENV setups

--- api/index.js
+++ api/index.js
@@ -1,9 +1,20 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.listen(port || 3000, () => { 
-    console.log("Server running on port ", port);
-})
+dotenv.config();
+/*Connecting to MongoDb*/
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log("Connected to MongoDB Database Successfully");
+    app.listen(port || 3000, () => {
+      console.log("Server running on port ", port);
+    });
+  })
+  .catch((err) => console.error(err));
+
+
