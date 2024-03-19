6f41ae1e354a553d0d1f31aac4421bbfc629b66f
Adebayo Ayowale Gbenga <haydexwisdom@gmail.com>
2024-03-19 11:16:40 +0000

    BACKEND: Done with signup route

--- api/controllers/signup.js
+++ api/controllers/signup.js
@@ -1,12 +1,23 @@
 import errHandler from "./errhandler.js";
+import User from "../models/userschema.js";
+import bcryptjs from "bcryptjs";
 
-const signup = (req, res, next) => {
-    console.log(req.body);
-    const { username, email, password } = req.body;
-    if (!username || !email || !password) { 
-        return next(errHandler("Incomplete Credentials", 400));
-    }
-    return res.status(200).json({"msg": "success"});
-}
+const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    return next(errHandler("Incomplete Credentials", 400));
+  }
+
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username: username, email: email, password: hashedPassword });
+  console.log(newUser, typeof newUser);
+  try {
+    await newUser.save();
+    return res.status(200).json({ message: "User Added Successfully" });
+  } catch (err) {
+    return next(err);
+  }
+};
 
 export default signup;
