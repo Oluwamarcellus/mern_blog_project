4e30eda4fc768e9c43540a3b133ebf04901bb914
Adebayo Ayowale Gbenga <haydexwisdom@gmail.com>
2024-03-27 11:49:31 +0000

    FRONTEND: Added sorting filter to the Admin Post page

--- frontend/src/components/AdminPagePost.jsx
+++ frontend/src/components/AdminPagePost.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function AdminPagePost() {
   const [posts, setPosts] = useState(null);
+  const user = useSelector((state) => state.user.activeUser);
 
-  const handleDelete = async (postId) => { 
+  const handleDelete = async (postId) => {
     const validate = confirm("Proceed with deleting post?");
-    if (validate && postId) { 
+    if (validate && postId) {
       try {
         const data = await fetch(`/api/post/deletepost/${postId}`, {
           method: "DELETE",
@@ -22,11 +24,13 @@ export default function AdminPagePost() {
         console.log(err.message);
       }
     }
-  }
+  };
 
-  const getPosts = async () => {
+  const handleFilter = async (filter) => {
     try {
-      const data = await fetch("/api/post/getpost");
+      const data = await fetch(
+        `/api/post/getpost${filter ? `?userId=${filter}` : ""}`
+      );
       const res = await data.json();
       if (!data.ok) {
         throw new Error(res.errorMessage);
@@ -38,11 +42,23 @@ export default function AdminPagePost() {
   };
 
   useEffect(() => {
-    getPosts();
+    handleFilter(user._id);
   }, []);
 
   return (
     <div className="p-4 grow">
+      <div className="w-full py-2 my-2 bg-gray-300/50 border rounded-lg px-2 text-center">
+        <select
+          className="outline-none text-sm bg-transparent"
+          onChange={(e) => handleFilter(e.target.value)}
+          name=""
+          id=""
+        >
+          <option value={user._id}>My posts</option>
+          <option value="">All posts</option>
+        </select>
+      </div>
+
       <div className="flex rounded-md gap-3 bg-gray-100/80 py-2 text-sm font-medium justify-between px-4">
         <h2 className="w-[20%]">DATE</h2>
         {/* <h2 className="w-[17%]">POST IMAGE</h2> */}
@@ -65,8 +81,18 @@ export default function AdminPagePost() {
                 <img className="rounded-lg h-10 w-16" src={post.postImage} />
               </h2> */}
               <h2 className="w-[45%] font-thin text-sm">{post.title}</h2>
-              <Link onClick={() => handleDelete(post._id)} className="w-[20%] text-red-500 cursor-pointer hover:text-red-300">Delete</Link>
-              <Link to={`/post/edit/${post._id}`} className="w-[15%] text-green-500 cursor-pointer hover:text-green-300">Edit</Link>
+              <Link
+                onClick={() => handleDelete(post._id)}
+                className="w-[20%] text-red-500 cursor-pointer hover:text-red-300"
+              >
+                Delete
+              </Link>
+              <Link
+                to={`/post/edit/${post._id}`}
+                className="w-[15%] text-green-500 cursor-pointer hover:text-green-300"
+              >
+                Edit
+              </Link>
             </div>
           );
         })}
